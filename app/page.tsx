57faa5f92e5e0bc6edf844838c94c3e09504b187
jieuning <balllling92@gmail.com>
2024-10-31 14:13:59 +0900

    프로젝트 생성

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import './styles/page.css'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="moewView"></div>
+      <nav className="buttonWrap">
+        <button className="selectBtn">?</button>
+      </nav>
+    </div>
+  )
+}
